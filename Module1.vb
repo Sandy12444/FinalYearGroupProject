73aeda1ea716c5c3aa2a0b1d4fc8e5d2730effd8
Sandy12444 <40840399+Sandy12444@users.noreply.github.com>
2019-03-05 06:35:42 +0000

    Add files via upload

---
+++ Module1.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim theNumber As Integer
+        theNumber = 7
+        'a loop
+        For x = 1 To 12
+            Console.WriteLine("     7 x " & x & " = " & (7 * x))
+        Next
+        'the end of the loop
+        Console.ReadKey() 'pause so user can see
+
+    End Sub
+
+End Module
